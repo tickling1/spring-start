5af35ffa56c4eb6ac9e77a812a065ccae7cb2beb
tickling1 <allworldreview0130@gmail.com>
2024-06-10 00:11:11 +0900

    Spring-06-10

---
+++ src/main/java/com/example/hello_spring/SpringConfig.java
@@ -0,0 +1,21 @@
+package com.example.hello_spring;
+
+import com.example.hello_spring.repository.MemberRepository;
+import com.example.hello_spring.repository.MemoryMemberRepository;
+import com.example.hello_spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 수동으로 스프링 빈 등록하기
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
---
+++ src/main/java/com/example/hello_spring/controller/HelloController.java
@@ -0,0 +1,52 @@
+package com.example.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    // 정적 컨텐츠
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!");
+        return "hello";
+    }
+
+    // MVC와 템플릿 엔진
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    // api 방식
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 객체가 오면 JSON 방식으로 데이터를 만들어서 반환 hello(name:입력값)
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
